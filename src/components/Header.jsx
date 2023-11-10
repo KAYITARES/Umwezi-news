ee331cbe8facdfc4198b904a481c94d5f832354d
KAYITARES <cyntkayitare@gmail.com>
2023-11-10 11:22:01 +0200

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,61 @@
-import logo from '../assets/logo.jpeg';
-import { Link } from 'react-router-dom';
+import logo from "../assets/logo.jpeg";
+import { Link } from "react-router-dom";
 
 function Header() {
-
     return (
         <>
             <nav className="nav">
                 <div className="logo">
-                    <img src={logo} alt='umwezi logo' />
-
+                    <img src={logo} alt="umwezi logo" />
                 </div>
                 <ul className="nav-links">
                     <li>
-                        <a href="tel:+250785627690" className='nav-links_link'>Tel:+250785627690</a>
+                        <a href="tel:+250785627690" className="nav-links_link">
+                            Tel:+250785627690
+                        </a>
                     </li>
                     <li>
-                        <Link to="#" className='nav-links_link btn'>Sign-In</Link>
+                        <Link to="#" className="nav-links_link btn">
+                            Sign-In
+                        </Link>
                     </li>
-
                 </ul>
-
             </nav>
-            <div className='category'>
-                <ul className='category-links'>
-                    <li><a href="@" className='category-links--link'>Ubuzima</a></li>
-                    <li><a href="@" className='category-links--link'>Politike</a></li>
-                    <li><a href="@" className='category-links--link'>Ubukungu</a></li>
-                    <li><a href="@" className='category-links--link'>Uburezi</a></li>
-                    <li><a href="@" className='category-links--link'>Iyobokamana</a></li>
-                    <li><a href="@" className='category-links--link'>imikino</a></li>
+            <div className="category">
+                <ul className="category-links">
+                    <li>
+                        <Link href="@" className="category-links--link">
+                            Ubuzima
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="@" className="category-links--link">
+                            Politike
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="@" className="category-links--link">
+                            Ubukungu
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="@" className="category-links--link">
+                            Uburezi
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="@" className="category-links--link">
+                            Iyobokamana
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="@" className="category-links--link">
+                            imikino
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </>
-
-
-
-    )
+    );
 }
 export default Header;
